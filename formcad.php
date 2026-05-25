c7ca03941ca88f4da456455afbb334a638621a68
Paulo Gomes <paulo.1999sep@gmail.com>
2026-05-25 00:06:05 -0300

    projeto final

---
+++ formcad.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Cadastro de Clientes</h1>
+
+<form method="POST">
+
+    <input type="text"
+    name="nome"
+    placeholder="Nome">
+
+    <br><br>
+
+    <input type="email"
+    name="email"
+    placeholder="Email">
+
+    <br><br>
+
+    <input type="text"
+    name="telefone"
+    placeholder="Telefone">
+
+    <br><br>
+
+    <button class="btn btn-primary type="submit"> Cadastrar </button>
+    <button class="btn btn-primary" type="reset">Limpar</button>
+
+
+</form>
+
+</body>
+</html>
+
+
+
+
+
+
+<?php
+
