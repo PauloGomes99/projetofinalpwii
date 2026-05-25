cf8ca4e0ac1e15048c8ed80e1e51a0cd8c1d622a
Paulo Gomes <paulo.1999sep@gmail.com>
2026-05-24 23:33:00 -0300

    projeto final

---
+++ cliente.php
@@ -0,0 +1,19 @@
+<?php
+
+
+public class {
+
+public string $nome;
+public string $email;
+protected string $telefone;
+
+
+public function __construct($nome, $email, $telefone) {
+
+$this->nome = $nome;
+$this->email = $email;
+$this->telefone = $telefone;
+
+} 
+
+}
